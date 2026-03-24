9566ce1468a9446cb53ae562a29e3eeff2379048
AaishaShahani2001 <shahani6360@gmail.com>
2026-03-24 16:45:43 +0530

    implement book appointment form with validation and UI layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ import CounselorDashboard from "./pages/counselor/CounselorDashboard";
 import StudentDashboard from "./pages/student/StudentDashboard";
 
 import Counsellors from "./pages/Counsellors";
+import BookAppointment from "./pages/BookAppointment";
 
 
 export default function App() {
@@ -52,6 +53,9 @@ export default function App() {
           <Route path="/counselor-dashboard" element={<CounselorDashboard />} />
           {/* Student Dashboard */}
           <Route path="/student-dashboard" element={<StudentDashboard />} />
+
+          {/* Book Appointment Page */}
+          <Route path="/book-appointment" element={<BookAppointment />} />
 
           {/* Counselor Listing page */}
           <Route path="/counsellors" element={<Counsellors />} />
